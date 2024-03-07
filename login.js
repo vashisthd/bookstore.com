5e49fa738edab49e029065f6443ec0388d7324b5
Deepak Vashisth <146873273+vashisthd@users.noreply.github.com>
2024-03-07 12:09:00 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,13 @@
+if(!localStorage.getItem("credentials")) localStorage.setItem("credentials", JSON.stringify([]));
+
+var arr = JSON.parse(localStorage.getItem("credentials"));
+console.log(arr)
+
+let user_name = document.getElementById("username");
+let user_paswrd = document.getElementById("password");
+
+let store_data = () => {
+  credentials.push({username: user_name, password: user_paswrd});
+  localStorage.setItem("credentials", JSON.stringify(arr))
+
+};
